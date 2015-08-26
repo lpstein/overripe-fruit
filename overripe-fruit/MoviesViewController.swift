c4b20143ac39f17bdb7a2ac14051fae2cae62fbf
Patrick Stein <lpstein@yahoo-inc.com>
2015-08-26 11:57:49 -0700

    Basic template

---
+++ overripe-fruit/MoviesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MoviesViewController.swift
+//  overripe-fruit
+//
+//  Created by Patrick Stein on 8/26/15.
+//  Copyright (c) 2015 patrick. All rights reserved.
+//
+
+import UIKit
+
+@objc(MoviesViewController)
+class MoviesViewController: UIViewController {
+  @IBOutlet weak var scrollView: UIScrollView!
+  var refreshControl: UIRefreshControl!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    refreshControl = UIRefreshControl()
+    refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
+    scrollView.insertSubview(refreshControl, atIndex: 0)
+
+    scrollView.contentSize.height = 900.0
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    
+  }
+}
