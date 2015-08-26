a322437e75474fb2212426c243338c293196e33d
Patrick Stein <lpstein@yahoo-inc.com>
2015-08-26 10:19:54 -0700

    Initial Commit

---
+++ overripe-fruitTests/overripe_fruitTests.swift
@@ -0,0 +1,36 @@
+//
+//  overripe_fruitTests.swift
+//  overripe-fruitTests
+//
+//  Created by Patrick Stein on 8/26/15.
+//  Copyright (c) 2015 patrick. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class overripe_fruitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
